d51fc9b9387c27a0af6ee4732b72464903c3a646
Luis <luis.banon@avantica.net>
2020-06-22 00:03:24 -0500

    Fix values

--- admin-one-vue-bulma-dashboard-master/src/components/GoogleMap.vue
+++ admin-one-vue-bulma-dashboard-master/src/components/GoogleMap.vue
@@ -15,7 +15,7 @@
     <gmap-map
       :center="center"
       :zoom="12"
-      style="width:100%;  height: 400px;"
+      style="width:100%;  height: 600px;"
     >
       <gmap-marker
         :key="index"
@@ -30,7 +30,7 @@
 <script>
 export default {
   name: 'GoogleMap',
-  data() {
+  data () {
     return {
       // default to Montreal to keep it simple
       // change this to whatever makes sense
@@ -38,38 +38,39 @@ export default {
       markers: [],
       places: [],
       currentPlace: null
-    };
+    }
   },
 
-  mounted() {
+  mounted () {
     this.geolocate()
   },
 
   methods: {
     // receives a place object via the autocomplete component
-    setPlace(place) {
+    setPlace (place) {
       this.currentPlace = place
     },
-    addMarker() {
+    addMarker () {
       if (this.currentPlace) {
         const marker = {
           lat: this.currentPlace.geometry.location.lat(),
           lng: this.currentPlace.geometry.location.lng()
-        };
+        }
         this.markers.push({ position: marker })
         this.places.push(this.currentPlace)
         this.center = marker
         this.currentPlace = null
       }
     },
-    geolocate: function() {
+    geolocate: function () {
       navigator.geolocation.getCurrentPosition(position => {
         this.center = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
-        };
-      });
+        }
+      })
     }
   }
-};
+}
+
 </script>
