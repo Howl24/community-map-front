58562e897eed88ec60ffb1b994f90afb09342a1d
Luis <luis.banon@avantica.net>
2020-06-22 00:03:46 -0500

    Add map to main js

--- admin-one-vue-bulma-dashboard-master/src/router/index.js
+++ admin-one-vue-bulma-dashboard-master/src/router/index.js
@@ -17,6 +17,17 @@ const routes = [
   },
   {
     meta: {
+      title: 'Maps'
+    },
+    path: '/maps',
+    name: 'maps',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tables" */ '../views/Maps.vue')
+  },
+  {
+    meta: {
       title: 'Tables'
     },
     path: '/tables',
