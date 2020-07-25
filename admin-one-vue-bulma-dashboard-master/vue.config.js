3048391e2aff533156fd14c5ce4d6747a54cc383
Daniel Matos <daniel.matos@tuxpas.com>
2020-07-25 16:18:06 -0500

    Add vue s3 config deploy

--- admin-one-vue-bulma-dashboard-master/vue.config.js
+++ admin-one-vue-bulma-dashboard-master/vue.config.js
@@ -11,5 +11,25 @@ module.exports = {
         moment: 'moment'
       }
     }
+  },
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'community-map',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'community-map-web',
+      createBucket: true,
+      staticHosting: false,
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
   }
 }
